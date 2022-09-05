9f3cab2e3853f812699e6b1f509570c8f8256727
Kiril-Pavlov <Kire_P@yahoo.com>
2022-09-05 18:03:29 +0200

    carousel logic done. some CSS added to carousel.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,36 +4,38 @@ import { Link } from 'react-router-dom'
 import "./Navbar.css"
 
 const Navbar = () => {
-  return (
-    <nav className='navbar-container'>
-        <h1 className='navbar-title'>
-            DUROTAN
-        </h1>
-        <div className='navbar-links'>
-            <Link to="/">
-                Home
-            </Link>
-            <Link to="/shop">
-                Shop
-            </Link>
-            <Link to="/products">
-                Product
-            </Link>
-            <Link to="/blog">
-                Blog
-            </Link>
-            <Link to="/pages">
-                Pages
-            </Link>
+    return (
+        <div>
+            <nav className='navbar-container'>
+                <h1 className='navbar-title'>
+                    DUROTAN
+                </h1>
+                <div className='navbar-links'>
+                    <Link to="/" className='navbar-link'>
+                        Home
+                    </Link>
+                    <Link to="/shop" className='navbar-link'>
+                        Shop
+                    </Link>
+                    <Link to="/products" className='navbar-link'>
+                        Product
+                    </Link>
+                    <Link to="/blog" className='navbar-link'>
+                        Blog
+                    </Link>
+                    <Link to="/pages" className='navbar-link'>
+                        Pages
+                    </Link>
+                </div>
+                <div className='navbar-languages'>
+                    EN FR
+                </div>
+                <div className='navbar-user'>
+                    react icons
+                </div>
+            </nav>
         </div>
-        <div className='navbar-languages'>
-            EN FR
-        </div>
-        <div className='navbar-user'>
-            react icons
-        </div>
-    </nav>
-  )
+    )
 }
 
 export default Navbar
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import {AiOutlinePlayCircle} from 'react-icons/ai'
+import "./Home.css"
 
 const Home = () => {
+
+  const [activeSlide, setActiveSlide] = useState("1")
+
+  const decrementSliderState = () => {
+    let activeSlideInt = Number(activeSlide);
+    if (activeSlideInt !== 1) {
+      setActiveSlide(String(activeSlideInt - 1))
+    } else {
+      setActiveSlide("1")
+    }
+  }
+
+  const incrementSliderState = () => {
+    let activeSlideInt = Number(activeSlide);
+    if (activeSlideInt !== 3) {
+      setActiveSlide(String(activeSlideInt + 1))
+    } else {
+      setActiveSlide("3")
+    }
+  }
+
   return (
-    <div>Home</div>
+    <div className='home-container'>
+      <div className='carousel-container'>
+        {activeSlide === "1" ? (
+          <div className='carousel-1-container'>
+            <h4 className='small-subtitle'>NEW ARRIVAL</h4>
+            <h2 className='carousel-title'>Basic Colours Comeback</h2>
+            <p className='carousel-paragraph'>Less is more never out of date. In last of theese years, basic colours will comeback, discover our collections and choose your style</p>
+            <div className='carousel-1-buttons'>
+              <button className='carousel-1-button'>SHOP NOW</button>
+              <div className='play-button'><AiOutlinePlayCircle /> <span>VIEW LOOKBOOK</span> </div>
+            </div>
+            <button onClick={decrementSliderState}>PREV</button>
+            <button onClick={incrementSliderState}>NEXT</button>
+          </div>
+        ) : activeSlide === "2" ? (
+          <div>Slider 2
+            <button onClick={decrementSliderState}>PREV</button>
+            <button onClick={incrementSliderState}>NEXT</button>
+          </div>
+        ) : activeSlide === "3" ? (
+          <div>Slider 3
+            <button onClick={decrementSliderState}>PREV</button>|
+            <button onClick={incrementSliderState}>NEXT</button>
+          </div>
+        ) : null
+        }
+      </div>
+    </div>
   )
 }
 
