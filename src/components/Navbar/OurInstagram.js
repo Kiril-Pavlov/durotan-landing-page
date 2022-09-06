9424c80a27da45c51bbaf5575fbb973ba68f9173
atletisimo <37110896+atletisimo@users.noreply.github.com>
2022-09-07 00:25:19 +0200

    aboutComponentsAdded

---
+++ src/components/Navbar/OurInstagram.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const OurInstagram = () => {
+  return <div>
+    <h3 className="followOurInsta">FOLLOW OUR INSTAGRAM</h3></div>;
+};
+
+export default OurInstagram;
---
+++ src/components/Navbar/OurProducts.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./OurProducts.css";
+const OurProducts = () => {
+  return (
+    <div>
+      <h2 className="ourProducts">Our Products</h2>
+    </div>
+  );
+};
+
+export default OurProducts;
