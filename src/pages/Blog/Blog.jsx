87755e848abb7605c757b28b42218583497b10d4
atletisimo <37110896+atletisimo@users.noreply.github.com>
2022-09-17 01:09:09 +0200

    Update Blog.jsx

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import './Blog.css';
-import blogPost from '..//../assets/blog_post.jpg';
-import OurJournal from '../../components/OurJournal'
+import React from "react";
+import "./Blog.css";
+import blogPost from "..//../assets/blog_post.jpg";
+import OurJournal from "../../components/OurJournal";
 const Blog = () => {
   return (
     <div>
-     <h1> Our Journal</h1>
-<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humou
+      <h1> Our Journal</h1>
+      <p>
+        There are many variations of passages of Lorem Ipsum available, but the
+        majority have suffered alteration in some form, by injected humou
       </p>
       <img src={blogPost} />
-      <OurJournal/>
+      <OurJournal />
+      <ul className="blogSearch">
+        <li>All</li>
+        <li>Inspiration</li>
+        <li>Lookbook</li>
+        <li>Tips&Tricks</li>
+        <li>News</li>
+        <li>Others</li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default Blog
+export default Blog;
